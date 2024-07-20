3f8db069e922f11d55d5b67f2de4514f7e1e8aa5
Forberjones <forberjones1@gmail.com>
2024-07-20 01:53:53 +0100

    Files and Folders

---
+++ src/data/services.js
@@ -0,0 +1,29 @@
+import image8 from '../assets/images/services/Remedial-Test-1.png';
+import image9 from '../assets/images/services/Fire-Alarm-Image-Test-1.png';
+import image10 from '../assets/images/services/Wiring-Test-1.png';
+import image11 from '../assets/images/services/Inspection-Test-1.png';
+
+const services = [
+  {
+    title: 'Fixed Wiring Inspection & Testing',
+    description: '• Fuse board changes (consumer unit) \n• Additional circuits \n• Remedial electrical work',
+    image: image10,
+  },
+  {
+    title: 'Remedial Electrical Work',
+    description: '• Fault finding & remedial work done in accordance with NICEIC unsatisfactory reports',
+    image: image8,
+  },
+  {
+    title: 'Fire Alarm & Emergency Light Testing',
+    description: '• 6 Monthly fire alarm tests \n• 12 Monthly emergency light tests',
+    image: image9,
+  },
+  {
+    title: 'Property Portfolio Landlord EICR & Condition Reports',
+    description: '• Property testing \n• Remedial works \n• NICEIC certifications \n• PRNS certifications',
+    image: image11,
+  },
+];
+
+export default services;
